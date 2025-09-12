ccd66cf36127a831bfb56c5036a7c9bcb19069ee
ikanuka <boumboumlucas13@gmail.com>
2025-09-12 17:15:44 +0200

    adding new task

---
+++ task_tracker_cli/cli.py
@@ -0,0 +1,21 @@
+# contains all CLI code
+import json
+#Adding a new task
+def add_task(task):
+    with open('data.json', 'r+') as file:
+        data = json.load(file)
+        data['tasks'].append(task)
+        file.seek(0)
+        json.dump(data, file, indent=4)
+
+#Listing all tasks
+
+
+if __name__ == "__main__":
+    
+    add_task("Apprendre Git")
+
+
+    with open('data.json', 'r') as f: 
+        data=json.load(f)
+        print(data)
