909c49701c5325b0f9bdec0158c63b7011c3bd1e
ikanuka <boumboumlucas13@gmail.com>
2025-09-16 13:33:07 +0200

    adding input function

--- task_tracker_cli/cli.py
+++ task_tracker_cli/cli.py
@@ -3,7 +3,7 @@ import json
 #Adding a new task
 def add_task(task):
     with open('data.json', 'r+') as file:
-        data = json.load(file)
+        data = json.load(file)          
         data['tasks'].append(task)
         file.seek(0)
         json.dump(data, file, indent=4)
@@ -15,10 +15,19 @@ def list_tasks():
         for task in data['tasks']:
             print(task)
 
+#Input from command line
+def input_tasks():
+    with open('data.json', 'r+') as file:
+        data =json.load(file)
+        task=input("Enter a new task: ")
+        data['tasks'].append(task)
+        file.seek(0)
+        json.dump(data, file, indent=4)
+
+
 if __name__ == "__main__":
-
-    list_tasks()
-
     with open('data.json', 'r') as f: 
         data=json.load(f)
         print(data)
+    list_tasks()
+    
